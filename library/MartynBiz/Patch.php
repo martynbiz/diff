545867dfdd29f9a7e7c88f9358fa1cf4b2504072
Martyn Bissett <martynbissett@yahoo.co.uk>
2015-05-14 00:34:45 +0900

    Add patch class to handle patching

---
+++ library/MartynBiz/Patch.php
@@ -0,0 +1,36 @@
+<?php
+
+class Text_Patch
+{
+    /**
+     * Applies a diff to a string -- untested yo
+     * 
+     * @param string $diff Diff
+     * @param string $text String to patch
+     * 
+     * @return string Patched string
+     * */
+    static function apply($diff, $text)
+    {
+        $tmpname = tempnam(sys_get_temp_dir(), "diff");
+        $outname = tempnam(sys_get_temp_dir(), "diff");
+        $tmp = fopen($tmpname, "w");
+        $out = fopen($outname, "r");
+        fwrite($tmp, $text.PHP_EOL);
+        $proc = proc_open(
+            'patch '.$tmpname.' -o '.$outname, array(
+               0 => array("pipe", "r"),
+               1 => array("pipe", "w"),
+               2 => array("pipe", "w")
+            ), $pipes
+        );
+        if (is_resource($proc)) {
+            fwrite($pipes[0], $diff);
+            fclose($pipes[0]);
+            stream_get_contents($pipes[1]);
+            $newText = stream_get_contents($out);
+            fclose($pipes[1]);
+            return $newText;
+        }
+    }
+}
